b4dbd1fbf0d5ba13d73569cc8e08e1fc1cd1f192
Yehezkiel Wiradhika <tsukikagemaikaka1@gmail.com>
2022-06-23 12:53:52 +0700

    add dynamically generated sites

---
+++ pages/article/[id]/index.js
@@ -0,0 +1,57 @@
+import Link from "next/dist/client/link";
+
+const article = ({ article }) => {
+  // const router = useRouter();
+  // const { id } = router.query;
+
+  return (
+    <>
+      <h1>{article.title}</h1>
+      <p>{article.body}</p>
+      <br />
+      <Link href="/">Go Back</Link>
+    </>
+  );
+};
+
+// combination of getStaticProps and getStaticPaths to dynamically
+// generate props with the data
+export const getStaticProps = async (context) => {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${context.params.id}`
+  );
+  const article = await res.json();
+
+  return {
+    props: {
+      article,
+    },
+  };
+};
+
+export const getStaticPaths = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const articles = await res.json();
+  const ids = articles.map((article) => article.id);
+  const paths = ids.map((id) => ({ params: { id: id.toString() } }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+// getServerSideProps = fetch data at the time of request
+// export const getServerSideProps = async (context) => {
+//   const res = await fetch(
+//     `https://jsonplaceholder.typicode.com/posts/${context.params.id}`
+//   );
+//   const article = await res.json();
+
+//   return {
+//     props: {
+//       article,
+//     },
+//   };
+// };
+export default article;
